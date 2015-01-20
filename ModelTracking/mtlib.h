a34b02775468f9373be4c3e4f653b81ed222ce5b
Nate Chodosh <nchodosh@gmail.com>
2015-01-20 16:43:23 -0500

    got rid of some redunant headers

--- ModelTracking/mtlib.h
+++ ModelTracking/mtlib.h
@@ -3,7 +3,7 @@
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/features2d/features2d.hpp"
 #include "opencv2/calib3d/calib3d.hpp"
-#include "opencv2/nonfree/nonfree.hpp"
+//#include "opencv2/nonfree/nonfree.hpp"
 #include <iostream>
 #include <stdlib.h>
 #include <string>
